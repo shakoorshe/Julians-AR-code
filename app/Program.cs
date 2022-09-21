54ee7bf1ce95821b8509987f0682e2a0a56f8181
shakoorshe <mattsy4c@gmail.com>
2022-09-21 17:14:06 -0400

    A

--- app/Program.cs
+++ app/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace HelloWorld
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            /* 
+            ++ -> Increment 
+            ++ -> De
+            */
+
+        }
+    }
+}
